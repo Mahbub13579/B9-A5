e98bab1374e89f68a5abc427aed0a9cd34383f10
Mahbub Alam <mahbub135712@gmail.com>
2024-02-19 19:58:46 +0600

    added function for seat and available seat and the seat number that are selected

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let seatArr = [];
+let unicArr = [];
+let count = 0;
+
+const seats = document.getElementsByClassName('seat');
+for (const seat of seats) {
+
+  seat.addEventListener('click', function (e) {
+    e.target.style.backgroundColor = '#1DD100';
+    e.target.style.color = '#fff';
+    const seat = e.target.innerText;
+    const seatLists = document.getElementById('seatReasive');
+    const countSeat = document.getElementById('countSeat');
+    const availableSeat = document.getElementById('seatsNum');
+    const seatNum = availableSeat.innerText;
+    const btnNext = document.getElementById('btnNext');
+
+    const setListCreate = document.createElement('div');
+    setListCreate.innerHTML = ` <div
+    class="flex justify-between text-base font-normal text-[#03071299]"
+  >
+    <p class="seatList">${seat}</p>
+    <p>Economoy</p>
+    <p>550</p>
+  </div>`;
+    seatLists.appendChild(setListCreate);
+    count = count + 1;
+    countSeat.innerText = count;
+    const availableS = parseInt(seatNum) - 1;
+    availableSeat.innerText = availableS;
+    btnNext.innerHTML = `<input
+    id="btnNextMain"
+    type="button"
+    value="Next"
+    class="bg-primary text-white border-2 border-solid border-primary hover:bg-[#fff0] hover:text-primary py-2 sm:py-3 w-full rounded-2xl text-xl font-extrabold cursor-pointer"
+  />`;
+    const seatList = document.getElementsByClassName('seatList');
+    let at = [];
+    for (const a of seatList) {
+      const arr = a.innerText;
+      at.push(arr);
+    }
+    console.log(at, seat);
+    const b = at.includes(seat);
+    console.log(b);
+   
+  });
+}
